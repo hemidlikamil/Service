59f1dc6d168598c1cb062bfc100d0940205d57cc
Kamil Həmidli <hemidov_1994@mail.ru>
2020-02-14 18:03:13 +0400

    Add project files.

---
+++ Service/Controllers/ServiceController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Http;
+
+namespace Service.Controllers
+{
+    public class ServiceController : ApiController
+    {
+        public IHttpActionResult Get(string value)
+        {
+            var arr = value.ToCharArray();
+            Array.Reverse(arr);
+            return Ok(new string(arr));
+        }
+    }
+}
